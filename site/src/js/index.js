e0a9ba6381f7468446599297139b97c5ed1ca6e7
Pablo Mederos <contacto@pablomederos.dev>
2022-12-04 14:50:43 -0300

    Correcciones de estilos. Además se sustituye el listener de scroll por un intersection observer.

--- site/src/js/index.js
+++ site/src/js/index.js
@@ -12,9 +12,47 @@ createApp({
         fetch(this.routesPath)
         .then(response => response.json())
         .then(data => this.repos = data)
+        .then( _ => 
+          footerIntersectionObserver())
     }
   },
   created() {
     this.getRepoData()
-  }
+  },
 }).mount('#app')
+
+const footerIntersectionObserver = () => {
+
+  const elementObserver = document.querySelector('.appBody')
+  let options = {
+    root: elementObserver,
+    rootMargin: '0px',
+    threshold: 0.1
+  }
+  
+  let observer = new IntersectionObserver(footerIntersectionObserverCallback, options);
+
+  const targets = document.querySelectorAll("div.videos")
+  
+  Array.from(targets)?.forEach(target => observer.observe(target))
+}
+
+const footerIntersectionObserverCallback = (entries, observer) => {
+
+  const footer = document.querySelector("footer")
+
+  if(entries?.length)
+  {
+    entries.forEach( entry => {
+      
+      if(entry.isIntersecting) {
+        if(footer?.classList.contains("show"))
+          footer?.classList.remove("show")
+      }
+      else {
+        if(!footer?.classList.contains("show"))
+          footer?.classList.add("show")
+      }
+    })
+  }
+}
